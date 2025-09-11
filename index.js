78729f0183fe469efccf9e3d318a0ab8ee60a418
Naledi M Motswiane <nmmotswiane@gmail.com>
2025-09-11 15:32:12 +0200

    Added a function that will start the server

--- index.js
+++ index.js
@@ -124,3 +124,8 @@ about = { ...about, ...req.body };
 res.json({message: "Pofile updated successfully", about});
 });
 
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
